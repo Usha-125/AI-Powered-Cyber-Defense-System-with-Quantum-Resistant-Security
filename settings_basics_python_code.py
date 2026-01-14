023bc0514fa3a8f4731d9186cdcd12642d3d6f9b
Usha-125 <ushasgowda125@gmail.com>
2026-01-14 13:04:49 +0530

    basic python variables input taking tuple learnt

--- settings_basics_python_code.py
+++ settings_basics_python_code.py
@@ -3,4 +3,20 @@ name='usha'
 feild='AI, CYBERSECURITY,QUANTUM COMPUTING'
 year=2026
 print(f"I am {name}, working in feild of {feild},from year {year}")
-
+#basic maths
+a=5
+b=6
+c=int(input())
+d=int(input())
+print('addition:',a+b)
+print('subtraction:',b-a)
+print('division:',c/d)
+#taking input fro user
+interest=input('enter your interest')
+print('my interest are',interest)
+age=int(input('enter your age:'))
+print(age)
+features=['name','to','from']
+print(features)
+for f in features:
+    print(f)
