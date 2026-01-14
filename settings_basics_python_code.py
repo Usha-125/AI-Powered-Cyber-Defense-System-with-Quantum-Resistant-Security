eef25a286e36efbc83ec097cf4b6a277a7259d55
Usha-125 <ushasgowda125@gmail.com>
2026-01-14 12:41:39 +0530

    Update basic settings and learning intro

--- settings_basics_python_code.py
+++ settings_basics_python_code.py
@@ -1,1 +1,6 @@
 print("learning AI, CYBERSECURITY,QUANTUM COMPUTING")
+name='usha'
+feild='AI, CYBERSECURITY,QUANTUM COMPUTING'
+year=2026
+print(f"I am {name}, working in feild of {feild},from year {year}")
+
