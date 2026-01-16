136f3eebf84db0284e5975da54fea0c5ac2c9c59
Usha-125 <ushasgowda125@gmail.com>
2026-01-16 23:38:25 +0530

    updated dataset separated features from labels

--- day3.py
+++ day3.py
@@ -1,13 +1,19 @@
 import pandas as pd
-data =pd.read_csv("network_data.csv")
-print('full dataset')
+
+# Load the dataset
+data = pd.read_csv("network_data.csv")
+print("Dataset Loaded Successfully!\n")
 print(data)
-print(data.head(3))
-print(data.info())
-print("\n--- Features (X) ---")
+
+# Separate features (X) and labels (Y)
 X = data.drop("label", axis=1)
+Y = data["label"]
+
+print("\n--- Features (X) ---")
 print(X)
 
 print("\n--- Labels (Y) ---")
-Y = data["label"]
 print(Y)
+
+print("\nNumber of records:", len(data))
+print("Number of features:", X.shape[1])
